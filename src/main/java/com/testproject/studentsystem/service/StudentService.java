85a4b1b485bdb3bd4f1c931dfd150751d93ae66f
Ishan-Vimukthi <ishanvimukthie@gmail.com>
2024-02-25 22:14:52 +0530

    u

--- src/main/java/com/testproject/studentsystem/service/StudentService.java
+++ src/main/java/com/testproject/studentsystem/service/StudentService.java
@@ -2,6 +2,9 @@ package com.testproject.studentsystem.service;
 
 import com.testproject.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudent();
 }
